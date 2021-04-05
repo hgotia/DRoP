05ebc92856bfaa4882bdac064bae369f59415e7d
hgotia <hanzelgotia@gmail.com>
2021-04-04 18:52:51 -1000

    Delete MVCs/DropDB directory

--- MVCs/DropDB/Appointments.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Appointment]
-(
-	[ApptId] INT IDENTITY(1,1) NOT NULL,
-	[Date_and_time] DATETIME NOT NULL,
-	[FirstName] VARCHAR(30) NOT NULL,
-	[LastName] VARCHAR(30) NOT NULL,
-	[PhoneNumber] VARCHAR(20) NOT NULL,
-	PRIMARY KEY CLUSTERED ([ApptId] ASC)
-)
