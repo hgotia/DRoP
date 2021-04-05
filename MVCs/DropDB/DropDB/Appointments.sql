0894f4f4777561dcd1d83c31c272812a129d0a68
hgotia <hanzelgotia@gmail.com>
2021-04-04 18:51:26 -1000

    Add files via upload

---
+++ MVCs/DropDB/DropDB/Appointments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Appointment]
+(
+	[ApptId] INT IDENTITY(1,1) NOT NULL,
+	[Date_and_time] DATETIME NOT NULL,
+	[FirstName] VARCHAR(30) NOT NULL,
+	[LastName] VARCHAR(30) NOT NULL,
+	[PhoneNumber] VARCHAR(20) NOT NULL,
+	PRIMARY KEY CLUSTERED ([ApptId] ASC)
+)
